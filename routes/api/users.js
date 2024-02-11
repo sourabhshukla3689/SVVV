cf07aec6c0d9e9320b0651ea34cfad0c44fc6ae6
SOURABH SHUKLA <89777378+sitaramradhabihari@users.noreply.github.com>
2024-02-11 16:42:51 +0530

    File Uploads

---
+++ routes/api/users.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { query, validationResult, check } = require('express-validator');
+const User = require('../models/User');
+router.post('/',[
+    check('name','Name is required').not().isEmpty(),
+    check('email','please include a valid email').isEmail(),
+    check(
+        'password',
+        'Please enter a password with 6 or more characters'
+    ).isLength({min:6})
+], async(req,res)=>
+{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+    
+    const {name,email,password} = req.body;
+
+    try{
+        let user = await User.findOne({email});
+        if(user){
+            return  res.status(400).json({errors: [{msg: 'User alreaddy exists'}]});
+        }
+        const avatar = gravatar.url(email,{
+            s:'200',
+            r: 'pg',
+            d: 'mm'
+        })
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+
+        });
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password,salt);
+
+       await user.save();
+
+
+
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload,config.get('jwtSecret'),{expiresIn:3600},(err,token)=>{
+          if(err) throw err;
+          res.json({token });
+        });
+
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+
+    
+   });
+
+module.exports = router;
